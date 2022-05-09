c4f7be25e991fa733091920175addb306bfa8510
Tal Alon <tal_alon@msn.com>
2022-05-09 17:41:10 +0200

    Add code for hue and luminosity

--- src/App.js
+++ src/App.js
@@ -4,21 +4,29 @@ import { useState } from 'react';
 
 function App() {
   const [color, setColor] = useState(randomColor);
+  const [hue, setHue] = useState(randomColor);
+  const [luminosity, setLuminosity] = useState(randomColor.luminosity);
+
   return (
     <div
       className="App"
       style={{
         fontWeight: 'bold',
-        fontSize: '40px',
       }}
     >
-      React Random Color Generator!
+      <div
+        style={{
+          fontSize: '40px',
+        }}
+      >
+        React Random Color Generator!
+      </div>
       <div>
         <br />
         <div
           style={{
             height: '200px',
-            backgroundColor: color,
+            backgroundColor: randomColor({ hue: hue, luminosity: luminosity }),
             color: 'black',
             fontWeight: 'bold',
             fontSize: '20px',
@@ -30,10 +38,34 @@ function App() {
           Generated Color: {color}
         </div>
         <br />
-        <button onClick={() => setColor(randomColor())}>Generate</button>
+        <button
+          style={{
+            display: 'block',
+            margin: 'auto',
+          }}
+          onClick={() => setColor(randomColor())}
+        >
+          Generate
+        </button>
+      </div>
+      <br />
+      <br />
+      <div
+        style={{
+          display: 'grid',
+          justifyContent: 'center',
+        }}
+      >
+        Choose a hue
+        <input onChange={(event) => setHue(event.currentTarget.value)} />
+        <br />
+        <br />
+        Choose a luminosity
+        <input onChange={(event) => setLuminosity(event.currentTarget.value)} />
       </div>
     </div>
   );
 }
+console.log(randomColor);
 
 export default App;
