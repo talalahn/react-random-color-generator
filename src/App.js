561b6aea679081e9103e5461f382135d149e8de1
Tal Alon <tal_alon@msn.com>
2022-05-09 15:42:16 +0200

    Add code to make random color generator work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 function App() {
-  return <div className="App" />;
+  const [color, setColor] = useState(randomColor);
+  return (
+    <div
+      className="App"
+      style={{
+        fontWeight: 'bold',
+        fontSize: '40px',
+      }}
+    >
+      React Random Color Generator!
+      <div>
+        <br />
+        <div
+          style={{
+            height: '200px',
+            backgroundColor: color,
+            color: 'black',
+            fontWeight: 'bold',
+            fontSize: '20px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          Generated Color: {color}
+        </div>
+        <br />
+        <button onClick={() => setColor(randomColor())}>Generate</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
