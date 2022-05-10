6369bf67f2a1a51bcaadbf9baa31a3a41456dc73
Tal Alon <tal_alon@msn.com>
2022-05-10 16:33:21 +0200

    Add code for box size input

--- src/App.js
+++ src/App.js
@@ -1,71 +1,129 @@
+/** @jsxImportSource @emotion/react */
+
 import './App.css';
+import { css } from '@emotion/react';
 import randomColor from 'randomcolor';
 import { useState } from 'react';
 
-function App() {
-  const [color, setColor] = useState(randomColor);
-  const [hue, setHue] = useState(randomColor);
-  const [luminosity, setLuminosity] = useState(randomColor.luminosity);
+const headerStyles = css`
+  font-weight: bold;
+  font-size: 40px;
+`;
 
+const colorBoxStyles = (color, height, width) => css`
+  height: ${height}px;
+  width: ${width}px;
+  margin: 0 auto;
+  background-color: ${color};
+  color: #000000;
+  font-weight: bold;
+  font-size: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 1s;
+`;
+const buttonStyles = css`
+  display: flex;
+  margin: 0 auto;
+`;
+const labelDivStyles = css`
+  display: grid;
+  /* text-align: center; */
+  justify-content: center;
+`;
+
+const separateInputStyles = css`
+  display: flex;
+  /* flex-direction: row; */
+  justify-content: space-evenly;
+`;
+
+function App() {
+  const [hue, setHue] = useState('');
+  const [luminosity, setLuminosity] = useState('');
+  const [color, setColor] = useState(
+    randomColor({ hue: hue, luminosity: luminosity }),
+  );
+  const [height, setHeight] = useState('300px');
+  const [width, setWidth] = useState('300px');
   return (
-    <div
-      className="App"
-      style={{
-        fontWeight: 'bold',
-      }}
-    >
-      <div
-        style={{
-          fontSize: '40px',
-        }}
-      >
-        React Random Color Generator!
-      </div>
+    <div className="App">
+      <div css={headerStyles}>React Random Color Generator!</div>
       <div>
         <br />
-        <div
-          style={{
-            height: '200px',
-            backgroundColor: randomColor({ hue: hue, luminosity: luminosity }),
-            color: 'black',
-            fontWeight: 'bold',
-            fontSize: '20px',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
+        <div className="colorBox" css={colorBoxStyles(color, height, width)}>
           Generated Color: {color}
         </div>
         <br />
         <button
-          style={{
-            display: 'block',
-            margin: 'auto',
-          }}
-          onClick={() => setColor(randomColor())}
+          css={buttonStyles}
+          onClick={() =>
+            setColor(randomColor({ hue: hue, luminosity: luminosity }))
+          }
         >
           Generate
         </button>
       </div>
       <br />
-      <br />
-      <div
-        style={{
-          display: 'grid',
-          justifyContent: 'center',
-        }}
-      >
-        Choose a hue
-        <input onChange={(event) => setHue(event.currentTarget.value)} />
+      <div>
+        <div css={separateInputStyles}>
+          <div>
+            <label css={labelDivStyles}>
+              Choose a hue:
+              <input
+                onChange={(event) => {
+                  setColor(
+                    randomColor({
+                      hue: event.currentTarget.value,
+                      luminosity: luminosity,
+                    }),
+                  );
+                  setHue(event.currentTarget.value);
+                }}
+              />
+            </label>
+          </div>
+          <div>
+            <label css={labelDivStyles}>
+              Choose a luminosity:
+              <input
+                onChange={(event) => {
+                  setColor(
+                    randomColor({
+                      hue: hue,
+                      luminosity: event.currentTarget.value,
+                    }),
+                  );
+                  setLuminosity(event.currentTarget.value);
+                }}
+              />
+            </label>
+          </div>
+        </div>
         <br />
         <br />
-        Choose a luminosity
-        <input onChange={(event) => setLuminosity(event.currentTarget.value)} />
+        <div css={separateInputStyles}>
+          <div>
+            <label css={labelDivStyles}>
+              Height of color box (in pixels):
+              <input
+                onChange={(event) => setHeight(event.currentTarget.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label css={labelDivStyles}>
+              Width of color box (in pixels):
+              <input
+                onChange={(event) => setWidth(event.currentTarget.value)}
+              />
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-console.log(randomColor);
 
 export default App;
